28515b50b868eba6133cd599d5c93826f6ebd157
Rustam Bikiteev <rust.bikiteev@gmail.com>
2023-05-05 10:29:20 +0200

    Initial commit

---
+++ src/test/java/com/aston/componentsystem/ComponentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aston.componentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
