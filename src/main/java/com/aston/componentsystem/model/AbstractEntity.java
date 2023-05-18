9b26bb8e276463df4dadf2e0d3b4ad8e342810af
Rustam Bikiteev <rust.bikiteev@gmail.com>
2023-05-18 17:07:56 +0300

    added entities for testing liquibase

---
+++ src/main/java/com/aston/componentsystem/model/AbstractEntity.java
@@ -0,0 +1,29 @@
+package com.aston.componentsystem.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+
+import java.io.Serializable;
+
+@MappedSuperclass
+@Data
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class AbstractEntity implements Serializable {
+
+
+    @Column(name = "is_removed", nullable = false)
+    boolean isRemoved = Boolean.FALSE;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+
+}
