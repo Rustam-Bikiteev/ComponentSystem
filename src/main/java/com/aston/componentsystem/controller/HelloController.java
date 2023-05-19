f6ff8f12cebf09c883af754a837abf67b686c7fb
TimKhasanov <timm.hasanov95@gmail.com>
2023-05-19 17:42:56 +0300

    vip

--- src/main/java/com/aston/componentsystem/controller/HelloController.java
+++ src/main/java/com/aston/componentsystem/controller/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @RequestMapping (method = RequestMethod.GET, value = "/hello")
     public String hello()  {
-        return "Hello World!";
+        return "Hello World! ";
     }
 }
