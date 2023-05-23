bc4a7b5acdcba3db85e5a91ef8734c3af623550a
Rustam Bikiteev <rustam.bikiteev@gmail.com>
2023-05-23 16:52:43 +0300

    correct naming Convention

--- src/main/java/com/aston/componentsystem/controller/WorkCardController.java
+++ src/main/java/com/aston/componentsystem/controller/WorkCardController.java
@@ -2,9 +2,7 @@ package com.aston.componentsystem.controller;
 
 import com.aston.componentsystem.model.WorkCard;
 import com.aston.componentsystem.service.WorkCardService;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,18 +15,22 @@ public class WorkCardController {
         this.workCardService = workCardService;
     }
 
+    @GetMapping("/workCards")
     public List<WorkCard> getAllWorkCards(){
         return workCardService.getAllWorkCards();
     }
 
+    @GetMapping("/workCard/{id}")
     public WorkCard getWorkCardById(@PathVariable int id){
         return workCardService.getWorkCardById(id);
     }
 
+    @PostMapping("/workCard")
     public void saveWorkCard(@RequestBody WorkCard workCard){
         workCardService.saveWorkCard(workCard);
     }
 
+    @DeleteMapping("/workCards/{id}")
     public void deleteWorkCard(@PathVariable int id){
         WorkCard workCard = workCardService.getWorkCardById(id);
         if (workCard != null){
