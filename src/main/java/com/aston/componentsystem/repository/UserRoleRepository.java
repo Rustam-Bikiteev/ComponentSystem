90519557bae92d642faacd084de154b32974015e
yuurm <73988712+yuurm@users.noreply.github.com>
2023-05-23 17:28:58 +0300

    add ROLE_ADMIN into liquidbase createRoles.yaml

--- src/main/java/com/aston/componentsystem/repository/UserRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aston.componentsystem.repository;
-import com.aston.componentsystem.model.UserRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface UserRoleRepository extends JpaRepository<UserRole, Integer> {
-    Optional<UserRole> findByName(String name);
-}
