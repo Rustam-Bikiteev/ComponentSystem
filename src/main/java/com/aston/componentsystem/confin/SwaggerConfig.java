d04babe3487088a658f737d32cceff7eaf8450a5
yuurm <73988712+yuurm@users.noreply.github.com>
2023-05-24 22:15:47 +0300

    add slf4j logging using lombok

--- src/main/java/com/aston/componentsystem/confin/SwaggerConfig.java
+++ src/main/java/com/aston/componentsystem/confin/SwaggerConfig.java
@@ -5,9 +5,11 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 @OpenAPIDefinition
 @SecurityScheme(
@@ -17,8 +19,11 @@ import org.springframework.context.annotation.Configuration;
     scheme = "bearer"
 )
 public class SwaggerConfig {
+    
+
     @Bean
     public OpenAPI openApi() {
+        log.info("Создание компонента OpenAPI bean");
         return new OpenAPI()
             .info(new Info()
                 .title("Component System"));
--- src/main/java/com/aston/componentsystem/security/CustomUserDetailsService.java
+++ src/main/java/com/aston/componentsystem/security/CustomUserDetailsService.java
@@ -2,6 +2,7 @@ package com.aston.componentsystem.security;
 
 import com.aston.componentsystem.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,12 +11,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        log.info("Получение пользователя по username: {}", login);
         return new CustomUserDetails(userRepository.findByLogin(login).orElseThrow(
                 () -> new UsernameNotFoundException("Пользователь с логином <" + login + "> не найден")));
     }
