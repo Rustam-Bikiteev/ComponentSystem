b3fd785c693bc7629f5ed6484aaef74765a87e17
Rustam Bikiteev <rustam.bikiteev@gmail.com>
2023-05-29 16:45:33 +0300

    hot fix to master

--- src/main/java/com/aston/componentsystem/config/SwaggerConfig.java
+++ src/main/java/com/aston/componentsystem/config/SwaggerConfig.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,7 +21,6 @@ public class SwaggerConfig {
     public OpenAPI openApi() {
         return new OpenAPI()
             .info(new Info()
-                .title("Component System"))
-                .addServersItem(new Server().url("https://componentsystem-production.up.railway.app"));
+                .title("Component System"));
     }
 }
